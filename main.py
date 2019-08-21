6d60dcf4d68b741b3fba498075022535eb63dcab
bhban <needleworm@kaist.ac.kr>
2019-08-21 17:33:30 +0900

    do not wait

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import sys
 import system_simulator as ss
 
 filename = sys.argv[1]
-network = ss.Network(filename, "result_fast_" + filename[:-4] + ".csv")
+network = ss.Network(filename, "result_" + filename[:-4] + ".csv")
 network.converge()
